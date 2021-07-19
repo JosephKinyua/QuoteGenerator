33285cccc7312f62454913854a89fa89224d90be
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-19 15:54:35 +0300

    add use of ngSubmit directive from angular forms module

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDescription: boolean;
-  constructor(public id: number,public title: string,public quote: string,public author: string,public submitted : string){
+  constructor(public id: number,public title: string,public quote: string,public author: string,public submitted : string,public completeDate:Date){
     this.showDescription=false;
 }
 }
