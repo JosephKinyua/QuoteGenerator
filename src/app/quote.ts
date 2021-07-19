e0a32342fb6c34f43d5365d44b6d22ca21508f65
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-19 20:24:09 +0300

    adding of the details by clicking add button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDescription: boolean;
-  constructor(public id: number,public title: string,public quote: string,public author: string,public submitted : string,public completeDate:Date){
+  constructor(public id: number,public title: string,public quote: string,public author: string,public submitted : string,public dateUpload: Date,){
     this.showDescription=false;
 }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,19 +9,19 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
   new Quote(1,'inspirational','You have gotta dance like there is nobody watching,Love like you will never be hurt,Sing like there is nobody listening,And live like it is heaven on earth.','William W. Purkey','john doe',new Date(2020,3,14)),
-  new Quote(1,'life','Life is what happens when you are busy making other plans.','John Lennon','Mary',new Date(2020,3,14)),
-  new Quote(1,'love','A man is already halfway in love with any woman who listens to him.','Brendan Francis','chris doe',new Date(2020,3,14))
+  new Quote(1,'life','Life is what happens when you are busy making other plans.','John Lennon','Mary',new Date(2019,6,9)),
+  new Quote(1,'love','A man is already halfway in love with any woman who listens to him.','Brendan Francis','chris doe', new Date(2022,1,12))
   ];
 
-  toggleDetails(index:any){
+  toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  completeQuote(isComplete:boolean, index:number){
+  completeQuote(isComplete, index){
     if (isComplete) {
       this.quotes.splice(index,1);
     }
   }
-  deleteQuote(isComplete:boolean, index:number){
+  deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].title}?`)
 
@@ -29,6 +29,12 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
