7dbb1a1748753910f1ee0583fea38f428d094d3a
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-19 14:10:41 +0300

    adding a click button with a function toggledetails

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,3 +3,4 @@ export class Quote {
   constructor(public id: number,public title: string,public quote: string,public author: string,public submitted : string){
     this.showDescription=false;
 }
+}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,9 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1,'inspirational','You have gotta dance like there is nobody watching,Love like you will never be hurt,Sing like there is nobody listening,And live like it is heaven on earth.','William W. Purkey','john doe'),
-new Quote(1,'life','Life is what happens when you are busy making other plans.','John Lennon','Mary'),
-new Quote(1,'love','A man is already halfway in love with any woman who listens to him.','Brendan Francis','chris doe')
+  new Quote(1,'inspirational','You have gotta dance like there is nobody watching,Love like you will never be hurt,Sing like there is nobody listening,And live like it is heaven on earth.','William W. Purkey','john doe'),
+  new Quote(1,'life','Life is what happens when you are busy making other plans.','John Lennon','Mary'),
+  new Quote(1,'love','A man is already halfway in love with any woman who listens to him.','Brendan Francis','chris doe')
   ];
   constructor() { }
 
