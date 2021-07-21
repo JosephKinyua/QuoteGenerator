d55ad307765ff0246fba32e32969917f0e1341c4
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-07-21 14:27:12 +0300

    commit

--- src/app/countdown.pipe.ts
+++ src/app/countdown.pipe.ts
@@ -9,25 +9,25 @@ export class CountdownPipe implements PipeTransform {
     let timeCount: string = "";
     let today: any = new Date();
     var difference = Math.abs(value - today) / 1000;
-    var days       = Math.floor(difference / 86400);
+    var days= Math.floor(difference / 86400);
     if(days > 0){
-      timeCount   += Math.floor(days) +" days ";
+      timeCount+= Math.floor(days) +" days ";
     }
-    difference    -= days * 86400;
-    var hours      = Math.floor(difference / 3600) % 24;
+    difference-= days * 86400;
+    var hours= Math.floor(difference / 3600) % 24;
     if(hours > 0){
-      timeCount   += Math.floor(hours)+" hrs ";
+      timeCount+= Math.floor(hours)+" hrs ";
     }
-    difference    -= hours * 3600;
+    difference-= hours * 3600;
 
-    var minutes    = Math.floor(difference / 60) % 60;
+    var minutes= Math.floor(difference / 60) % 60;
     if(minutes > 0){
-      timeCount   += Math.floor(minutes)+" mins ";
+      timeCount+= Math.floor(minutes)+" mins ";
     }
-    difference    -= minutes * 60;
-    var seconds    = difference % 60;
+    difference -= minutes * 60;
+    var seconds = difference % 60;
     if(seconds > 0){
-      timeCount  += Math.floor(seconds)+" secs ";
+      timeCount+= Math.floor(seconds)+" secs ";
     }
     return timeCount;
   }
